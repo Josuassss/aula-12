0034423949a3d7169dce8850170a6e79cc22fa80
Josuassss <josuesathlersantos@gmail.com>
2024-11-23 12:00:19 -0300

    Tarefas

--- examples/index.js
+++ examples/index.js
@@ -50,7 +50,7 @@ console.log(allEven);
 
 /* 
 const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-for (let i = 0; i < arr.length, i++) {
+for (let i = 0; i < arr.length; i++) {
   console.log(arr[i]);
 }
 */
